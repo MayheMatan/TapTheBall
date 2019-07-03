5afbca37cd0bdbb2b4aef1c2e1e17dd07ac47eae
myusername <Matanfriedleet@hotmail.com>
2019-07-04 00:49:44 +0300

    fixes added

---
+++ app/src/main/java/com/mayhematan/taptheball/MusicManager.java
@@ -0,0 +1,32 @@
+package com.mayhematan.taptheball;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+public class MusicManager { // Singleton Class
+    private static MusicManager reference = null;
+    private MediaPlayer mediaPlayer;
+
+    public static MusicManager getInstance() {
+        if (reference == null) {
+            reference = new MusicManager ();
+        }
+        return reference;
+    }
+
+    public void initalizeMediaPlayer (Context context, int musicId) {
+        mediaPlayer = new MediaPlayer ();
+        mediaPlayer.release ();
+        mediaPlayer = MediaPlayer.create ( context, musicId );
+        mediaPlayer.setLooping ( true );
+    }
+
+    public void startMusic() {
+        mediaPlayer.start ();
+    }
+
+    public void stopMusic() {
+        mediaPlayer.stop ();
+        mediaPlayer.release ();
+    }
+}
